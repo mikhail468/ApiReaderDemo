2809677f0d4389ec051659c3c548cbf4c9c00613
Mikhail Dushin <mikhail.dushin@yahoo.com>
2018-07-22 13:42:35 +1000

    Importing project

---
+++ WindowsFormsApp1/BinarySorting.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class BinarySorting
+    {
+        private void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(3);
+            list.Add(9);
+            list.Add(4);
+            list.Add(15);
+            list.Add(5);
+            list.Add(7);
+            list.Add(2);
+            string s = "";
+            foreach (int x in list)
+            {
+                s = s + x + "\n";
+            }
+            Console.WriteLine(s);
+
+        }
+
+        public List<int> Sorting(List<int> elements)
+        {
+            string aI;
+            string aJ;
+
+            for (int i = 0; i < elements.Count; i++)
+            {
+                aI = Convert.ToString(elements[i], 2);
+                for (int j = i + 1; j < elements.Count; j++)
+                {
+                    aJ = Convert.ToString(elements[j], 2);
+                    if (aJ.Length - aJ.Replace("1", "").Length > aI.Length - aI.Replace("1", "").Length)
+                    {
+                        int s = elements[i];
+                        elements[i] = elements[j];
+                        elements[j] = s;
+                        aI = Convert.ToString(elements[i], 2);
+                        continue;
+                    }
+                    if (aJ.Length - aJ.Replace("1", "").Length == aI.Length - aI.Replace("1", "").Length)
+                    {
+                        if (elements[j] < elements[i])
+                        {
+                            int d = elements[i];
+                            elements[i] = elements[j];
+                            elements[j] = d;
+                            aI = Convert.ToString(elements[i], 2);
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            return elements;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static ApiResult result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = "https://api.bitbucket.org/2.0/repositories";
+            result = _downloadSerializedData<ApiResult>(s);
+            string msg = "";
+            foreach(Value x in result.values)
+            {
+                msg = msg + x.Name + "\n";
+            }
+            label1.Text = msg;
+        }
+
+        private static T _downloadSerializedData<T>(string url) where T:new() {
+            using(var w = new WebClient())
+            {
+                var json_data = string.Empty;
+                try
+                {
+                    json_data = w.DownloadString(url);
+                }
+                catch { }
+                return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
+            }
+        }
+
+        public class ApiResult
+        {
+            public List<Value> values { get; set; }
+        }
+        public class Value
+        {
+            public string Name { set; get; }
+        }
+    }
+}
